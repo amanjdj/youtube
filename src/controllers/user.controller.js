f29e2902c7084d696807852e13672c1bc79e95c1
Aman Sharma <amanjdj@gmail.com>
2025-08-25 15:29:57 +0530

    bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -395,7 +395,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 });
 
 const getWatchHistory = asyncHandler(async (req, res) => {
-  const user = User.aggregate([
+  const user = await User.aggregate([
     {
       $match: {
         _id: new mongoose.Types.ObjectId(rq.user._id),
