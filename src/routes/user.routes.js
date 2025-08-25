7f7b078122feea5d7c812feefd222d1010d72312
Aman Sharma <amanjdj@gmail.com>
2025-08-25 15:49:07 +0530

    fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -50,11 +50,11 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 router
   .route("/avatar")
-  .patch(verifyJWT, upload.single("/avatar"), updateUserAvatar);
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 router
   .route("/cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
